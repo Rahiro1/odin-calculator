7c4dd9c76f7ff519afe5ace56ef0cd3587099b59
James <jdt130@bham.ac.uk>
2025-04-14 15:15:17 +0100

    Implemented simple equals logic and bugfix
    changed second / button to +
    fixed typo in operator functions num2 instead of numTwo
    Added equals button logic

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let firstNumber = 0;
-let secondNumber = 0;
 let operatorToUse = "";
 const pDisplay = document.querySelector(".display-p")
 const btnNumButtonsList = document.querySelectorAll(".number-button");
 const btnFuncButtonsList = document.querySelectorAll(".function-button");
+const btnEquals = document.querySelector(".equals-button");
 
 btnNumButtonsList.forEach(function (currentValue, currentIndex, listObj){
     currentValue.addEventListener("click", function(e) {clickNumberButton(e)})
@@ -12,6 +12,8 @@ btnNumButtonsList.forEach(function (currentValue, currentIndex, listObj){
 btnFuncButtonsList.forEach(function (currentValue, currentIndex, listObj){
     currentValue.addEventListener("click", function(e) {clickFunctionButton(e)})
 });
+
+btnEquals.addEventListener("click", function(e) {clickEqualsButton(e)})
 
 function clickNumberButton(e){
     let button = e.target;
@@ -36,39 +38,47 @@ function clickFunctionButton(e){
 
 }
 
+function clickEqualsButton(e){
+    let button = e.target;
+    let displayText = pDisplay.textContent;
+
+    if(!isNaN(displayText)){
+        let secondNumber = Number(displayText);
+        pDisplay.textContent = operate(firstNumber,secondNumber,operatorToUse);
+        firstNumber = secondNumber;
+    }
+}
+
 function operate(numOne, numTwo, operator){
     switch(operator) {
     case "+":
         return add(numOne,numTwo);
     case "-":
-        return add(numOne,numTwo);
-        break;
+        return subtract(numOne,numTwo);
         case "*":
-            return add(numOne,numTwo);
-        break;
+            return multiply(numOne,numTwo);
         case "/":
-            return add(numOne,numTwo);
-        break;
+            return divide(numOne,numTwo);
     default:
         return 0;
     }
 }
 
 function add(numOne, numTwo){
-    return numOne + num2;
+    return numOne + numTwo;
 }
 
 function subtract(numOne, numTwo){
-    return numOne - num2;
+    return numOne - numTwo;
 }
 
 function multiply(numOne, numTwo){
-    return numOne * num2;
+    return numOne * numTwo;
 }
 
 function divide(numOne, numTwo){
     if(numTwo == 0){
         return null;
     }
-    return numOne / num2;
+    return numOne / numTwo;
 }
